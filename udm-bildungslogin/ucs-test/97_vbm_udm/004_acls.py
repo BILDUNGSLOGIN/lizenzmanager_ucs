48f0ba7905230a39b27c0f16d08bb06626ad02f9
Jürn Brodersen <brodersen@univention.de>
2021-08-24 12:29:54 +0200

    Issue #38: ucslint: Variable uses internal Python keyword: 'license'

--- udm-bildungslogin/ucs-test/97_vbm_udm/004_acls.py
+++ udm-bildungslogin/ucs-test/97_vbm_udm/004_acls.py
@@ -47,24 +47,24 @@ ucr.load()
 
 def test_license_acl_machine(create_license):
     code = "CODE"
-    license = create_license(code, "PRODUCT_ID", 10, "DEMOSCHOOL")
+    bilo_license = create_license(code, "PRODUCT_ID", 10, "DEMOSCHOOL")
     lo, _ = getMachineConnection()
     if ucr.get("server/role") in ["domaincontroller_master", "domaincontroller_backup"]:
-        assert lo.searchDn(base=license.dn)
+        assert lo.searchDn(base=bilo_license.dn)
     else:
         with pytest.raises(noObject):
-            lo.searchDn(base=license.dn)
+            lo.searchDn(base=bilo_license.dn)
 
 
 def test_license_acl_user(create_license):
     code = "CODE"
-    license = create_license(code, "PRODUCT_ID", 10, "DEMOSCHOOL")
+    bilo_license = create_license(code, "PRODUCT_ID", 10, "DEMOSCHOOL")
     user_pw = "univention"
     with udm_test.UCSTestUDM() as udm:
         userdn, username = udm.create_user(password=user_pw)
         lo = access(binddn=userdn, bindpw=user_pw, base=ucr.get("ldap/base"))
         with pytest.raises(noObject):
-            lo.searchDn(base=license.dn)
+            lo.searchDn(base=bilo_license.dn)
 
 
 @pytest.mark.skipif(
