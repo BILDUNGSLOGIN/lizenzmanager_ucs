2abab35de360dfa77733f2de5eb09df93c7d31db
Frank Greif <greif@univention.de>
2022-08-10 11:39:33 +0200

    Fix wrong distinction between Error and No-Error condition.

--- python-bildungslogin/src/univention/bildungslogin/media_import/cmd_media_update.py
+++ python-bildungslogin/src/univention/bildungslogin/media_import/cmd_media_update.py
@@ -73,33 +73,35 @@ def update_ldap_meta_data(lo):  # type: (Any) -> bool
     product_ids_in_ldap = {o.product_id for o in mh.get_all()}
     print("A total of {} product IDs was found in LDAP.".format(len(product_ids_in_ldap)))
     product_ids_to_update = set(updated_product_ids).intersection(product_ids_in_ldap)
-    if len(product_ids_to_update):
-        print(
-            "Going to retrieve {} meta data entries from the media server...".format(
-                len(product_ids_to_update)
-            )
-        )
-        raw_media_data = retrieve_media_data(
-            access_token, config["resource_server"], sorted(product_ids_to_update)
-        )
-        error_message = import_multiple_raw_media_data(mh, raw_media_data)
-        # store earliest update timestamp from a successful item download
-        all_dates_unique = {data["data"]["modified"] for data in raw_media_data if data["status"] == 200}
-        if all_dates_unique:
-            save_last_update_timestamp(min(all_dates_unique))
-        if error_message:
-            print(error_message)
-            print("-" * 80)
-            print("The next update will nevertheless look for updates from 'now' on.")
-            print("To manually update products meta data, run:")
-            print(
-                "bildungslogin-media-import --config-file /etc/bildungslogin/config.ini product_id "
-                "[product_id ...]"
-            )
-            print("Or delete {!r} to redownload all previous updates.".format(UPDATE_TIMESTAMP_FILE))
-            return False
-    else:
+    # No products in LDAP -> fall through -> ERROR
+    # Some products in LDAP but nothing to update -> regular flow and nothing to do -> NO ERROR
+    if len(product_ids_in_ldap) and not len(product_ids_to_update):
         print("No updates available to the products known to this system.")
+        return True
+    print(
+        "Going to retrieve {} meta data entries from the media server...".format(
+            len(product_ids_to_update)
+        )
+    )
+    raw_media_data = retrieve_media_data(
+        access_token, config["resource_server"], sorted(product_ids_to_update)
+    )
+    error_message = import_multiple_raw_media_data(mh, raw_media_data)
+    # store earliest update timestamp from a successful item download
+    all_dates_unique = {data["data"]["modified"] for data in raw_media_data if data["status"] == 200}
+    if all_dates_unique:
+        save_last_update_timestamp(min(all_dates_unique))
+    if error_message:
+        print(error_message)
+        print("-" * 80)
+        print("The next update will nevertheless look for updates from 'now' on.")
+        print("To manually update products meta data, run:")
+        print(
+            "bildungslogin-media-import --config-file /etc/bildungslogin/config.ini product_id "
+            "[product_id ...]"
+        )
+        print("Or delete {!r} to redownload all previous updates.".format(UPDATE_TIMESTAMP_FILE))
+        return False
     print("No errors happened downloading the meta data.")
     return True
 
