80e725e468bdb3328b0452bf8c6c5f74a16be337
ebarq <ernesto.barquin@bergwerk-it.de>
2023-04-20 13:04:16 +0200

    feature(umc): Reimplement the module license overview.

--- ucs-school-umc-licenses/umc/js/licenses/modules/licenses.js
+++ ucs-school-umc-licenses/umc/js/licenses/modules/licenses.js
@@ -1,15 +1,41 @@
 define([
   'dojo/_base/declare',
-  'umc/widgets/Module',
+  'dojo/_base/lang',
+  'dojox/html/entities',
   'umc/widgets/Text',
-
-], function(declare, Module, Text) {
+  '../common/Module',
+  './licenses/SearchPage',
+  './licenses/DetailPage',
+  'umc/i18n!umc/modules/licenses',
+], function(declare, lang, entities, Text, Module, SearchPage, DetailPage, _) {
   return declare('umc.modules.licenses.licenses', [Module], {
+
+    searchPage: null,
+    pages: [],
+
+    openDetailPage: function(licenseId) {
+      this.selectPage(2);
+      this.currentPage().load(this.getSchoolId(), licenseId);
+    },
 
     buildRendering: function() {
       this.inherited(arguments);
-      this._text = new Text({content: 'licenses'});
-      this.addChild(this._text);
+      const searchPage = new SearchPage({
+        getSchoolId: lang.hitch(this, 'getSchoolId'),
+        openDetailPage: lang.hitch(this, 'openDetailPage'),
+      });
+      this.addPage(searchPage);
+
+      const detailPage = new DetailPage({
+        standbyDuring: lang.hitch(this, 'standbyDuring')
+      });
+      this.addPage(detailPage);
     },
+
+    // buildRendering: function() {
+    //   this.inherited(arguments);
+    //   this._text = new Text({content: 'licenses'});
+    //   this.addChild(this._text);
+    // },
   });
 });
